842c3d770f5b360a7a3946c6be0f63104621262e
Arlo Virta <35749102+arlojay@users.noreply.github.com>
2023-05-08 14:33:54 +0000

    add back original code because i dont know how branches and prs work

---
+++ main.js
@@ -0,0 +1,47 @@
+const startTime = Date.now();
+const target = new Date("5/8/2023, 8:00:00 AM").getTime();
+
+let nextBoomTime = 0;
+let lastWholeSecond = 0;
+
+function numAdvance() {
+    const boom = new Audio("vine-boom.mp3");
+    boom.play();
+}
+
+function getTimeRemaining(t) {
+    const ct = (startTime + t);
+    const dt = target - (startTime + t);
+
+    time.style.setProperty("--pulseval", (dt % 1000) / 500);
+
+    let [num, dec] = (Math.floor(dt) / 1000).toString().split(".");
+    if(dec == null) dec = "0";
+
+    if(lastWholeSecond != Math.floor(ct / 1000)) {
+        if(lastWholeSecond != 0) {
+            document.body.dataset.started = "true";
+        }
+        lastWholeSecond = Math.floor(ct / 1000);
+        numAdvance();
+    }
+
+    dec = dec.padStart(3, "0");
+    if(dec.length > 3) dec = dec.slice(0, 3);
+
+    return num + "." + dec + "s";
+}
+
+function update() {
+    try {
+        timeval.textContent = getTimeRemaining(performance.now());
+    } catch(e) {}
+
+    requestAnimationFrame(update);
+}
+
+function init() {
+    requestAnimationFrame(update);
+}
+
+init();
