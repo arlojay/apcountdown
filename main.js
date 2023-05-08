5497206ad6b2eeb077c50cc5373bea255a449435
Arlo Virta <35749102+arlojay@users.noreply.github.com>
2023-05-08 09:45:00 -0500

    oops 3
    i made it for 8am instead of 12am

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const startTime = Date.now();
-const target = new Date("5/8/2023, 8:00:00 AM").getTime();
+const target = new Date("5/8/2023, 12:00:00 AM").getTime();
 
 let nextBoomTime = 0;
 let lastWholeSecond = 0;
